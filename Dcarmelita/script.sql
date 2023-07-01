1fdd4113d21880925a923dcbc12a4116886936dc
toencina <107225390+toencina@users.noreply.github.com>
2023-07-01 17:47:21 -0400

    Add files via upload

--- Dcarmelita/script.sql
+++ Dcarmelita/script.sql
@@ -9,20 +9,12 @@ CREATE TABLE producto(
     valor INT NOT NULL
 );
 
-INSERT INTO producto (id,imagen,nombre,descripcion,valor) VALUES (1,'pan de nuez.webp','Pan De Campo Nuez','Pan de campo de masa madre con nueces molidas',3500)
-INSERT INTO producto (id,imagen,nombre,descripcion,valor) VALUES (2,'pan brioche.webp','Pan Brioche','Esponjoso pan dulce hecho de huevo y leche',3000)
-INSERT INTO producto (id,imagen,nombre,descripcion,valor) VALUES (3,'pan de chocolate.webp','Pan De Chocolate','Pan de masa madre dulce sabor chocolate',4000)
-INSERT INTO producto (id,imagen,nombre,descripcion,valor) VALUES (4,'pan de centeno.webp','Pan De Centeno','Pan de molde integral con semillas',2000)
-INSERT INTO producto (id,imagen,nombre,descripcion,valor) VALUES (5,'empanada pino.webp','Empanada de Pino','Empanada de horno con carne picada',3000)
-INSERT INTO producto (id,imagen,nombre,descripcion,valor) VALUES (6,'cheescake.webp','Cheescake Chocolate','Cheescake de crema pastelera y de chocolate',2500)
-INSERT INTO producto (id,imagen,nombre,descripcion,valor) VALUES (7,'mufin de chocolate.webp','Muffin Chocolate ','Muffin de chocolate, con centro de chocolate',1900)
-INSERT INTO producto (id,imagen,nombre,descripcion,valor) VALUES (8,'palmeritas.webp','Palmeritas','Palmetas crujientes, y cubiertas por caramelo',1000)
-
--- CREACION DE TABLA CONTACTO
-CREATE TABLE contacto(
-    id INT PRIMARY KEY,
-    nombre VARCHAR(120) NOT NULL,
-    apellido VARCHAR(120) NOT NULL,
-    mail VARCHAR(120) NOT NULL,
-    contrasena VARCHAR(120)NOT NULL
-);
+INSERT INTO producto (id,imagen,nombre,descripcion,valor) VALUES 
+(1,'img/pan de nuez.webp','Pan De Campo Nuez','Pan de campo de masa madre con nueces molidas',3500),
+(2,'img/pan brioche.webp','Pan Brioche','Esponjoso pan dulce hecho de huevo y leche',3000),
+(3,'img/pan de chocolate.webp','Pan De Chocolate','Pan de masa madre dulce sabor chocolate',4000),
+(4,'img/pan de centeno.webp','Pan De Centeno','Pan de molde integral con semillas',2000),
+(5,'img/empanada pino.webp','Empanada de Pino','Empanada de horno con carne picada',3000),
+(6,'img/cheescake.webp','Cheescake Chocolate','Cheescake de crema pastelera y de chocolate',2500),
+(7,'img/mufin de chocolate.webp','Muffin Chocolate ','Muffin de chocolate, con centro de chocolate',1900),
+(8,'img/palmeritas.webp','Palmeritas','Palmetas crujientes, y cubiertas por caramelo',1000);
